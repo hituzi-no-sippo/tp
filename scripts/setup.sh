3ec72d13afb541deccff30ec3afa9a1741508e33
hituzi no sippo <dev@hituzi-no-sippo.me>
2023-07-17 06:59:24 +0900

    build(git-hook): add setup for Git hook to setup shell script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# @(#) v0.1.0 2023-07-13T06:55:34+0900
+# @(#) v0.2.0 2023-07-13T06:55:34+0900
 # @(#) Copyright (C) 2023 hituzi-no-sippo
 # @(#) LICENSE: MIT-0 (https://choosealicense.com/licenses/mit-0/)
 
@@ -23,12 +23,24 @@ install_tools_with_version_manager() {
 
   info 'Successful lazy install. Download a tool when that is executed.'
 }
+setup_git_hook() {
+  info "Setup Git hooks"
+
+  if ! lefthook install --aggressive; then
+    err 'Failed Git hooks setup.'
+
+    return 1
+  fi
+
+  info 'Successful install Git hooks.'
+}
 
 main() {
   info 'Setup Start'
 
   funcs=(
     'install_tools_with_version_manager'
+    'setup_git_hook'
   )
 
   for func in "${funcs[@]}"; do
