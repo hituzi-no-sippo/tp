7d4d0f3f7e8aa5743a4ac8e8b1ebc1aa7966209b
hituzi no sippo <dev@hituzi-no-sippo.me>
2023-08-10 05:59:49 +0900

    ci(file-path): add Git hook to detect file path conflict in not case sensitive

---
+++ .lefthook/file_path/detect_conflict_in_not_case_sensitive/detect_conflict_in_not_case_sensitive.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+#
+# @(#) v0.1.0 2023-08-09T12:50:58+0900
+# @(#) Copyright (C) 2023 hituzi-no-sippo
+# @(#) LICENSE: MIT-0 (https://choosealicense.com/licenses/mit-0/)
+
+detect_case_conflict() {
+  echo "$@" |
+    tr ' ' '\n' |
+    sort --ignore-case --dictionary-order |
+    uniq --ignore-case --all-repeated=separate
+}
+
+main() {
+  conflicting_text=$(detect_case_conflict "$@")
+
+  if [ "$conflicting_text" = '' ]; then
+    exit 0
+  fi
+
+  echo "$conflicting_text"
+
+  exit 1
+}
+
+main "$@"
