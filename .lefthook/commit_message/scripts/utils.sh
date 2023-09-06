2fab5f7d104c261722abf61cd409bd0b55a576a3
hituzi no sippo <dev@hituzi-no-sippo.me>
2023-09-06 20:23:22 +0900

    ci(commit-msg): add script to extract Git commit message from `git log` command

---
+++ .lefthook/commit_message/scripts/utils.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+#
+# @(#) v0.1.0 2023-11-11T08:44:09+0900
+# @(#) Copyright (C) 2023 hituzi no sippo
+# @(#) LICENSE: MIT-0 (https://choosealicense.com/licenses/mit-0/)
+
+err() {
+  printf '[%s]: %s\n' "$(date +'%Y-%m-%dT%H:%M:%S%z')" "$*" >&2
+}
+
+request_hash_for_last_push_or_remote_HEAD() {
+  last_push="$(git rev-parse @\{push\})"
+
+  if [ "$last_push" != '' ]; then
+    printf '%s' "$last_push"
+
+    return 0
+  fi
+
+  request_hash_for_remote_HEAD "$1"
+}
+request_hash_for_remote_HEAD() {
+  git ls-remote "$1" HEAD | cut --fields=1
+}
+# Run Command
+# `git log --pretty=format:'%B' <REVISION_RANGE>`
+# `<REVISION_RANGE>` is `<@{push}|REMOTE_HEAD_BRANCH}>..HEAD`
+extract_commit_message_from_HEAD_to_last_push_or_remote_HEAD() {
+  hash="$(request_hash_for_last_push_or_remote_HEAD "$1")"
+
+  if [ "$hash" = '' ]; then
+    err "Could not fetch the hash for '@{push}' or remote HEAD."
+
+    return 1
+  fi
+
+  # Commit Range
+  # References
+  # - https://git-scm.com/book/en/v2/Git-Tools-Revision-Selection#Commit-Ranges
+  # - https://git-scm.com/book/en/v2/Git-Tools-Revision-Selection#_double_dot
+  extract_commite_message "$hash..HEAD"
+}
+
+extract_commite_message() {
+  git log --pretty=format:'%B' "$1"
+}
